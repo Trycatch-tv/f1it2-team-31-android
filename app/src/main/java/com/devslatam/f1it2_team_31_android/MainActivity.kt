826d49663ffa6206c015d6b58efe162bc13b3caa
barbosaivan <ibarbosao@ufpso.edu.co>
2023-05-26 18:55:24 -0500

    Build: Creación del proyecto base en Kotlin para Android con nivel de API mínimo 5.0.

---
+++ app/src/main/java/com/devslatam/f1it2_team_31_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.devslatam.f1it2_team_31_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
