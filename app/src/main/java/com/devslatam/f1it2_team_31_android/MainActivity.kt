80654f30590f8a103abcc91476794273448f0260
barbosaivan <ibarbosao@ufpso.edu.co>
2023-05-26 21:32:49 -0500

    Feat: creación de la interfaz de inicio de la aplicación.

--- app/src/main/java/com/devslatam/f1it2_team_31_android/MainActivity.kt
+++ app/src/main/java/com/devslatam/f1it2_team_31_android/MainActivity.kt
@@ -2,10 +2,25 @@ package com.devslatam.f1it2_team_31_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.devslatam.f1it2_team_31_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setUpButtons()
+    }
+
+    private fun setUpButtons() {
+        binding.btnLogin.setOnClickListener {
+
+        }
+        binding.btnRegister.setOnClickListener {
+
+        }
     }
 }
